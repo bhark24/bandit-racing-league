96d5c77ffd6f8b2006e107e72cdd60cde82b0839
bhark24 <bhark24@users.noreply.github.com>
2026-06-02 21:28:02 -0400

    Merge and sync tracker roster list changes with website database

--- roster_data.js
+++ roster_data.js
@@ -1,4 +1,8 @@
 const rosterData = {
+  "00": {
+    "driver": "Jackson Knaak",
+    "status": "full-time"
+  },
   "1": {
     "driver": "RICKY HART",
     "status": "past-champion"
@@ -7,9 +11,17 @@ const rosterData = {
     "driver": "NICK NICKERSON",
     "status": "past-champion"
   },
+  "02": {
+    "driver": "NOLAN GROSS",
+    "status": "part-time"
+  },
   "4": {
     "driver": "LOGAN MURRAY",
     "status": "full-time"
+  },
+  "04": {
+    "driver": "BRET GUZIK",
+    "status": "past-champion"
   },
   "5": {
     "driver": "SEAN BRITT",
@@ -75,12 +87,20 @@ const rosterData = {
     "driver": "MICHAEL RAMOS",
     "status": "full-time"
   },
+  "28": {
+    "driver": "Jesse VAUGHAN",
+    "status": "part-time"
+  },
   "43": {
     "driver": "JONATHON PLATT",
     "status": "past-champion"
   },
   "47": {
     "driver": "MICHAEL RAKES",
+    "status": "full-time"
+  },
+  "50": {
+    "driver": "DAVIS CARROLL",
     "status": "full-time"
   },
   "51": {
@@ -130,18 +150,6 @@ const rosterData = {
   "99": {
     "driver": "JOSH BILLITER",
     "status": "full-time"
-  },
-  "00": {
-    "driver": "Jackson Knaak",
-    "status": "full-time"
-  },
-  "02": {
-    "driver": "NOLAN GROSS",
-    "status": "part-time"
-  },
-  "04": {
-    "driver": "BRET GUZIK",
-    "status": "past-champion"
   }
 };
 const numbersList = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"];
